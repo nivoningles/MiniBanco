306bf9c4efe29f71db54430d9a38ff5014dece94
nivoningles <contadonivon@gmail.com>
2024-11-27 20:41:18 -0300

    segundo commit

--- src/Interacoes.java
+++ src/Interacoes.java
@@ -1,4 +1,79 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Interacoes {
+
+    private Double valorFinal = 500.;
+    private List<String> mensagem = new ArrayList<String>();
+
+
+    public void operacao(String valor, int operacao) {
+
+        try {
+
+
+
+        if (operacao == 1) {
+
+            if (valorFinal < Double.valueOf(valor)) {
+
+                mensagem.add("Erro Saldo Insuficiente");
+
+            } else if (Double.valueOf(valor) < 0) {
+
+                mensagem.add("Erro Numero negativo");
+
+            } else {
+
+                mensagem.add("Saque Bem Sucedido");
+                valorFinal = valorFinal - Double.valueOf(valor);
+
+            }
+
+        } else if (operacao == 2) {
+
+            if (Double.valueOf(valor) >= 1000) {
+
+                mensagem.add("Erro Limite Maximo de Deposito é 1000");
+
+            }else if (Double.valueOf(valor) < 0) {
+
+                mensagem.add("Erro Numero negativo");
+
+            } else {
+
+                mensagem.add("Deposito Bem Sucedido");
+                valorFinal = valorFinal + Double.valueOf(valor);
+
+            }
+
+
+        }
+
+    }catch (Exception e) {
+
+            mensagem.add("Informar Valor Valido");
+
+        }
+
+    }
+
+    public String getSaldo(){
+        return "R$ " + valorFinal;
+    }
+
+
+    public String mensagem(){
+
+        return String.join("\n", mensagem);
+
+    }
+
+    public void limpar(){
+
+        mensagem.clear();
+
+    }
+
+
 }
--- src/SistemaBancario.java
+++ src/SistemaBancario.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class SistemaBancario {
+    private JLabel txtSaldo;
+    private JTextField txtfSaque;
+    private JButton btnSaque;
+    private JTextField txtfDeposito;
+    private JButton btnDeposito;
+    public JTextArea txtArea;
+    private JButton btnLimpar;
+    private JPanel panelTela;
+    private JPanel penalPrimeiro;
+    private JPanel panelSegundo;
+    private JPanel panelSegundoEsquerda;
+    private JPanel panelSegundoDireita;
+    private JPanel panelTerceiro;
+
+    Interacoes inter = new Interacoes();
+
+    public SistemaBancario() {
+        btnSaque.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (txtfSaque.getText().equals("")) {
+
+                    txtfSaque.setText("");
+
+                }else {
+
+                    inter.operacao(txtfSaque.getText(), 1);
+                    txtSaldo.setText(inter.getSaldo());
+                    txtArea.setText(inter.mensagem());
+                    txtfSaque.setText("");
+
+                }
+
+            }
+        });
+        btnDeposito.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (txtfDeposito.getText().equals("")) {
+
+                    txtfDeposito.setText("");
+
+                }else {
+
+                    inter.operacao(txtfDeposito.getText(), 2);
+                    txtSaldo.setText(inter.getSaldo());
+                    txtArea.setText(inter.mensagem());
+                    txtfDeposito.setText("");
+                }
+
+            }
+        });
+        btnLimpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                txtArea.setText("");
+                inter.limpar();
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Sistema Bancario");
+        frame.setContentPane(new SistemaBancario().panelTela);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
 }
+
+
+
+
